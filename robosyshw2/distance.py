681129951a9f624144f234c31157db576a14d7ad
Hayato Tsukada <szhongtain85@gmail.com>
2025-12-30 14:07:12 +0900

    Add a command

--- robosyshw2/distance.py
+++ robosyshw2/distance.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # SPDX-FileCopyrightText: 2025 Hayato Tsukada
 # SPDX-License-Identifier: BSD-3-Clause
+
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Point
@@ -11,7 +12,6 @@ class DistanceSubscriber(Node):
         self.sub = self.create_subscription(Point, 'mouse_pos', self.cb, 10)
 
     def cb(self, msg):
-        # 距離計算をやめて、座標(X, Y)をそのまま表示する
         self.get_logger().info(f'Position: X={msg.x:.0f}, Y={msg.y:.0f}')
 
 def main():
